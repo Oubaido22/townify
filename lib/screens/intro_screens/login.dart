1381a51624da29afefd052bdfed963ea3872f46a
oubeid <oubaidloukil@gmail.com>
2023-11-21 15:49:36 +0100

    add signup and forget pass UI

--- lib/screens/intro_screens/login.dart
+++ lib/screens/intro_screens/login.dart
@@ -5,17 +5,19 @@ import 'package:classchool/screens/intro_screens/add_new_student.dart';
 import 'package:classchool/screens/intro_screens/welcome.dart';
 import 'package:classchool/screens/main/main_page.dart';
 import 'package:classchool/screens/studentprofile.dart';*/
-import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+//import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'signup.dart';
+//import 'signup.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:townify/screens/intro_screens/forget_password.dart';
+import 'package:townify/screens/intro_screens/signup.dart';
 import 'package:townify/screens/intro_screens/welcome.dart';
 //import 'forget_password.dart';
 import '../../utils/submit_button.dart' show Submit_Button;
 import '../../utils/textfield.dart' show CustomTextField, PasswordField;
-import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:awesome_dialog/awesome_dialog.dart';
 
@@ -23,7 +25,7 @@ import 'package:awesome_dialog/awesome_dialog.dart';
 class Login extends StatelessWidget {
   Login({Key? key}) : super(key: key);
   final _formKey = GlobalKey<FormState>();
-  final _auth = FirebaseAuth.instance;
+  //final _auth = FirebaseAuth.instance;
   late String email;
   late String pass = '';
   var isLoading = false;
@@ -39,7 +41,7 @@ class Login extends StatelessWidget {
   TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    void route() {
+    /*void route() {
       User? user = FirebaseAuth.instance.currentUser;
       var kk = FirebaseFirestore.instance
           .collection('users')
@@ -92,7 +94,7 @@ class Login extends StatelessWidget {
           }
         }
       }
-    }
+    }*/
 
     return Material(
       child: Scaffold(
@@ -173,7 +175,7 @@ class Login extends StatelessWidget {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => Welcome(),
+                            builder: (context) => forgetPassword(),
                           ),
                         );
                       },
@@ -184,7 +186,7 @@ class Login extends StatelessWidget {
                       fontSize: 22,
                       ontouch: () {
                         print('trying to sign in');
-                        signIn(email, pass);
+                        //signIn(email, pass);
                         print('sign in successufl');
                       },
                     ),
